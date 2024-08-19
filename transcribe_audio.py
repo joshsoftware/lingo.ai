ba3ffd02fe0619ef4442c57409ff26dec441adcf
Sethupathi Asokan <sethu@joshsoftware.com>
2024-08-19 21:44:27 +0530

    Revamped the whole project, now you can record or upload a audio file

--- transcribe_audio.py
+++
@@ -1,5 +0,0 @@
-def transcribe_audio(model, audio_data):
-    options = dict(beam_size=5, best_of=5)
-    transcribe_options = dict(task="transcribe", **options)
-    transcript = model.transcribe(audio_data, **transcribe_options)
-    return transcript["text"]
