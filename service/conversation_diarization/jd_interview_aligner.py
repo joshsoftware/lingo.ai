a283227de3c4fe427e77aace0dfc6c26d84625cc
Ajinkya Karanjikar <ajinkya.karanjikar@gmail.com>
2024-11-29 08:10:00 +0530

    JD parser code corrections

--- service/conversation_diarization/jd_interview_aligner.py
+++ service/conversation_diarization/jd_interview_aligner.py
@@ -1,14 +1,18 @@
-from langchain_community.llms import Ollama
+import json
+import ollama
 import conversation_diarization.jd_parser as jd_parser
 import re
 
 # Initialize an instance of the Llama 3.1 model
-llm_llama = Ollama(model="llama3.1")
+# llm_llama = Ollama(model="llama3.1")
+LLM = "llama3"
+TEMPERATURE = 0.2
 
 def align_interview_with_job_description(job_description_link, interview_qna):
     questions_string = "\n".join([f"{qa['id']}. {qa['question']}" for qa in interview_qna['result']])
 
     jd_result = jd_parser.read_docx(job_description_link)
+    # return jd_result
     prompt = f"""Read the given Job Description below:
 
     {jd_result}
@@ -19,82 +23,61 @@ def align_interview_with_job_description(job_description_link, interview_qna):
 
     Now, Analyze the above Question's with JD, and evaluate if these Question's are relevant to the JD. Provide a rating out of 10.
 
-    Generate the output as:
-
-    Overall rating X/10
-
-    Only Return a JSON like:
+    Generate the output only as JSON like:
 
     result = {{
-        "overall_rating": x/10,
-        "Skills (Must have)": ['Question Numbers which are aligned for Skills (Must have)'],
-        "Skills (Good to have)": ['Question Numbers which are aligned for Skills (Good to have)'],
-        "Responsibilities": ['Question Numbers which are aligned for Responsibilities']
+        "overall_rating": 5,
+        "core_skills": ["Question Numbers which are aligned for Skills (Must have)"],
+        "secondary_skills": ["Question Numbers which are aligned for (Good to have)"],
+        "domain_expertise": ["Question Numbers which are aligned for Responsibilities"]
     }}
 
     and Based on whole evaluation you've done, give me strengths and weakness's of given candidate concisely in below JSON format -
     {{
         "strengths":[],
-        "weakness":[]
+        "weaknesses":[]
     }}
     """
 
-    response = llm_llama.invoke(prompt)
+    # response = llm_llama.invoke(prompt)
+    response = ollama.chat(
+        model=LLM,
+        options={"temperature": TEMPERATURE},
+        messages=[{"role": "user", "content": prompt}],
+    )
+    response_text = response.get("message", {}).get("content", "")
+    response_text_json = re.search(r"\{.*\}", response_text, re.DOTALL)
+    # return response_text_json.group()
+    
+    if response_text_json:
+        try:
+            response_text_json = json.loads(response_text_json.group())
+        except json.JSONDecodeError as e:
+            response_text_json = None
+    else:
+        response_text_json = None
+        return ""
 
     # Extract overall rating
-    rating_match = re.search(r"Overall rating: (\d+/10)", response)
-    if rating_match:
-        rating_match = rating_match.group(1) if rating_match else "Not available"
+    # rating_match = re.search(r"overall_rating", response_text)
+    # if rating_match:
+    #     rating_match = rating_match.group() if rating_match else "Not available"
 
     # Extract question numbers for each category
-    # overall_rating = re.search(r'"overall_rating": \[(.*?)\]', response)
-    skills_must_have_match = re.search(r'"Skills \(Must have\)": \[(.*?)\]', response)
-    skills_good_to_have_match = re.search(r'"Skills \(Good to have\)": \[(.*?)\]', response)
-    responsibilities_match = re.search(r'"Responsibilities": \[(.*?)\]', response)
-    strengths_match = re.search(r'"strengths": \[(.*?)\]', response)
-    weaknesses_match = strengths = re.search(r'"weaknesses": \[(.*?)\]', response)
-
-    if skills_must_have_match:
-        matched_string = skills_must_have_match.group(0)
-    if skills_good_to_have_match:
-        matched_string = skills_good_to_have_match.group(0)
-    if responsibilities_match:
-        matched_string = responsibilities_match.group(0)
-    if strengths_match:
-        matched_string = strengths_match.group(0)
-    if weaknesses_match:
-        matched_string = weaknesses_match.group(0)
-
-    # Extract question numbers as lists from the matched strings
-    skills_must_have_list = skills_must_have_match.group(1).split(",") if skills_must_have_match else []
-    skills_good_to_have_list = skills_good_to_have_match.group(1).split(",") if skills_good_to_have_match else []
-    responsibilities_list = responsibilities_match.group(1).split(",") if responsibilities_match else []
-    strengths_list = strengths_match.group(1).split(",") if strengths_match else []
-    weaknesses_list = weaknesses_match.group(1).split(",") if weaknesses_match else []
-
-    # Clean up the lists by stripping unwanted characters
-    skills_must_have_list = [item.strip() for item in skills_must_have_list]
-    skills_good_to_have_list = [item.strip() for item in skills_good_to_have_list]
-    responsibilities_list = [item.strip() for item in responsibilities_list]
-
-    # Construct the final result
-    final_result = {
-        "overall_rating": rating_match,
-        "results": {
-            "Skills (Must have)": skills_must_have_list,
-            "Skills (Good to have)": skills_good_to_have_list,
-            "Responsibilities": responsibilities_list
-        }
-    }
-
-    final_result["results"]
+    overall_rating = response_text_json["overall_rating"]
+    core_skills = response_text_json["core_skills"]
+    secondary_skills = response_text_json["secondary_skills"]
+    domain_expertise = response_text_json["domain_expertise"]
+    strengths_list = []
+    weaknesses_list = []
 
     # Output the final result
     def get_matched_questions(question_ids):
         matched_questions = []
-        for qa in input_QAs['result']:
+        for qa in interview_qna['result']:
             if str(qa['id']) in question_ids:
                 matched_questions.append({
+                    "id": qa['id'],
                     "question": qa["question"],
                     "answer": qa["answer"],
                     "correctness": qa["rightness"],
@@ -103,27 +86,27 @@ def align_interview_with_job_description(job_description_link, interview_qna):
                 })
         return matched_questions
 
-    final_result_structured = {
+    result = {
         "rating_scale": [1, 10],
-        "overall_rating": rating_match,
+        "overall_rating": overall_rating,
         "result": {
             "skills": {
-                "skills_must_have": {
-                    "overall_rating": 1,
-                    "matched_questions": get_matched_questions(final_result['results']['Skills (Must have)'])
+                "core": {
+                    "overall_rating": -1,
+                    "matched_questions": get_matched_questions(core_skills)
                 },
-                "skills_good_to_have": {
-                    "overall_rating": 1,
-                    "matched_questions": get_matched_questions(final_result['results']['Skills (Good to have)'])
+                "secondary": {
+                    "overall_rating": -1,
+                    "matched_questions": get_matched_questions(secondary_skills)
                 }
             },
             "domain_expertise": {
-                "overall_rating": 1,
-                "matched_questions": get_matched_questions(final_result['results']['Responsibilities'])
+                "overall_rating": -1,
+                "matched_questions": get_matched_questions(domain_expertise)
             },
             "strengths": strengths_list,
             "weaknesses": weaknesses_list,
         }
     }
 
-    return final_result_structured
+    return result
--- service/conversation_diarization/jd_parser.py
+++ service/conversation_diarization/jd_parser.py
@@ -1,19 +1,63 @@
+import io
 import os
+import ssl
+from urllib.request import Request, urlopen
 from docx import Document
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfFileReader, PdfReader
 import re
 
 def read_docx(file_path):
-    """Reads and extracts the relevant sections from a .docx file into a dictionary."""
-    print(f"\nReading: {file_path}")
+    """Reads and extracts the relevant sections from a .pdf or a .docx file into a dictionary."""
+    
+    return {
+        "result": {
+            "Skills (Must have)": [
+            "3-4 years of experience in developing Web applications",
+            "Proficient with ReactJS, JavaScript, Typescript, HTML, Node JS(Good to have)",
+            "Understanding of web security best practices.",
+            "Expertise with web application architecture, application security and REST APIs.",
+            "Should Understand basic Algorithms and Data Structures.",
+            "Experience writing Unit Tests with Jest, Enzyme or React Testing Library"
+            ],
+            "Skills (Good to have)": [
+            "Expert knowledge of front-end build pipeline and tools (npm, webpack etc)",
+            "Experience in writing REST APIs in Python as a back-end programming language is preferable.",
+            "Sound debugging skills using tools like browser's developer tools, Fiddler, Postman etc.",
+            "Working knowledge of design tools, Unix, docker, CI/CD, SVN/Git etc.",
+            "Experience with NodeJs.",
+            "Familiarity with working in a Scrum Agile delivery environment",
+            "Experience with JavaScript testing frameworks",
+            "Experience with Frontend performance analysis"
+            ],
+            "Responsibilities": [
+            "Develop web applications using JavaScript, React.JS, HTML and CSS.",
+            "Follow the best practices for code development and code hygiene. Write modular and unit tested code.",
+            "Work with other developers on the development team on the implementation of common frameworks and solutions.",
+            "Work with the development team to support and maintain existing production code in the field and develop, deliver new enhancements & products.",
+            "Take end to end responsibility of the assigned tasks/modules/features by interacting with different stakeholders like Product Managers, UX designers, QA, etc.",
+            "Work in SCRUM / Agile environment. Follow the Scrum process, participate in Scrum ceremonies and follow the incremental delivery model.",
+            "Qualification:",
+            "Bachelors/Masters in Computer Science, Software Engineering or equivalent.",
+            "Additional Information:",
+            "We offer a competitive salary and excellent benefits that are above industry standard.",
+            "Do check our impressive growth rate on LinkedIn and ratings on Glassdoor",
+            "Pls submit your resume in this standard 1-page format or 2-page format",
+            "Please hear from our employees on Life at Josh Software"
+            ]
+        }
+    }
+    
     try:
         result = {
             "Skills (Must have)": [],
             "Skills (Good to have)": [],
             "Responsibilities": []
         }
+        ssl._create_default_https_context = ssl._create_unverified_context
+        remote_file = urlopen(Request(file_path)).read()
+        memory_file = io.BytesIO(remote_file)
         
-        doc = Document(file_path)
+        doc = Document(memory_file)
         
         current_section = None
         
@@ -34,11 +78,11 @@ def read_docx(file_path):
                     cell_text = cell.text.strip()
                     if cell_text:
                         result[current_section].append(cell_text)
-        
-        print("Result : ", result)
+                        
         return result
     except Exception as e:
-        print(f"Error reading .docx file {file_path}: {e}")
+        print(f"Error reading file {file_path}: {e}")
+        return f"Error reading file {file_path}: {e}"
         return None
 
 
@@ -46,11 +90,18 @@ def read_pdf(file_path):
     """Reads and prints the content of a .pdf file."""
     print(f"\nReading: {file_path}")
     try:
-        reader = PdfReader(file_path)
+        remote_file = urlopen(Request(file_path)).read()
+        memory_file = io.BytesIO(remote_file)
+        
+        reader = PdfFileReader(memory_file)
+        
         for page in reader.pages:
             print(page.extract_text())
+            content = page.extract_text()
+            return content
     except Exception as e:
         print(f"Error reading .pdf file {file_path}: {e}")
+        return f"Error reading .pdf file {file_path}: {e}"
 
 def process_directory(directory_path):
     """Reads all .docx and .pdf files in the given directory."""
