b561e3b7a6e7ce8749f04c37eff2e513e545783e
RohanDhalpe05 <rohan.dhalpe@joshsoftware.com>
2025-01-17 15:36:33 +0530

    Updated transcriptions to disply current user logged in transcriptions

--- app/src/app/api/transcriptions/route.ts
+++ app/src/app/api/transcriptions/route.ts
@@ -8,43 +8,59 @@ export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 export const revalidate = 0;
 
-
 export async function GET(req: NextRequest) {
-    try {
+  try {
+    const cursor = parseInt(req.nextUrl.searchParams.get("cursor") || "0");
+    const filter = req.nextUrl.searchParams.get("filter") || "false";
+    const userId = req.nextUrl.searchParams.get("userId") || null;
 
-        const cursor = parseInt(req.nextUrl.searchParams.get('cursor') || '0');
-        const filter = req.nextUrl.searchParams.get('filter') || 'false';
-
-        
-        if (filter !== 'true' && filter !== 'false' && filter !== null) {
-            return new Response(JSON.stringify({ error: "Invalid filter value. Must be 'true' or 'false'." }), { status: 400 });
-        }
-
-        const isFilter = filter === 'true';
-
-        const limit = PAGINATION_LIMIT;
-
-        const transcriptionsData = await db
-            .select({
-                id: transcriptions.id,
-                documentName: transcriptions.documentName,
-                createdAt: transcriptions.createdAt,
-                documentUrl: transcriptions.documentUrl,
-                isDefault: transcriptions.isDefault,
-                audioDuration: transcriptions.audioDuration,
-            })
-            .from(transcriptions)
-            .where(eq(transcriptions.isDefault, isFilter))
-            .orderBy(desc(transcriptions.createdAt))
-            .offset(cursor)
-            .limit(limit);
-
-        const nextCursor = transcriptionsData.length === limit ? cursor + limit : null;
-
-        return new Response(JSON.stringify({ transcriptions: transcriptionsData, nextCursor }), { status: 200 });
-
-    } catch (error) {
-        console.log(error);
-        return new Response(JSON.stringify(error), { status: 500 });
+    if (filter !== "true" && filter !== "false" && filter !== "user") {
+      return new Response(
+        JSON.stringify({
+          error: "Invalid filter value. Must be 'true', 'false', or 'user'.",
+        }),
+        { status: 400 }
+      );
     }
+
+    const isFilter = filter === "true";
+    const limit = PAGINATION_LIMIT;
+
+    let query = db
+      .select({
+        id: transcriptions.id,
+        documentName: transcriptions.documentName,
+        createdAt: transcriptions.createdAt,
+        documentUrl: transcriptions.documentUrl,
+        isDefault: transcriptions.isDefault,
+        audioDuration: transcriptions.audioDuration,
+      })
+      .from(transcriptions)
+      .where(
+        filter === "user" && userId
+          ? eq(transcriptions.userID, userId)
+          : eq(transcriptions.isDefault, isFilter)
+      )
+      .orderBy(desc(transcriptions.createdAt))
+      .offset(cursor)
+      .limit(limit);
+
+    const transcriptionsData = await query;
+
+    const nextCursor =
+      transcriptionsData.length === limit ? cursor + limit : null;
+    const hasTranscriptions = transcriptionsData.length > 0;
+
+    return new Response(
+      JSON.stringify({
+        transcriptions: transcriptionsData,
+        nextCursor,
+        hasTranscriptions,
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
 }
--- app/src/hooks/useTranscriptions.ts
+++ app/src/hooks/useTranscriptions.ts
@@ -2,20 +2,31 @@ import { userTranscriptions } from "@/types/transcriptions";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchTranscriptions = async ({ pageParam = 0, filter = "false" }) => {
-  const response = await axios.get(`/api/transcriptions?cursor=${pageParam}&filter=${filter}`);
+const fetchTranscriptions = async ({
+  pageParam = 0,
+  filter = "false",
+  userId = null as string | null | undefined,
+}) => {
+  const response = await axios.get(
+    `/api/transcriptions?cursor=${pageParam}&filter=${filter}&userId=${userId}`
+  );
   return response.data;
 };
 
-export const useTranscriptions = (initialData: userTranscriptions[], filter: string) => {
+export const useTranscriptions = (
+  initialData: userTranscriptions[],
+  filter: string,
+  userId: string | null
+) => {
   return useInfiniteQuery({
-    queryKey: ["transcriptions", filter],
-    queryFn: ({ pageParam = 0 }) => fetchTranscriptions({ pageParam, filter }),
+    queryKey: ["transcriptions", filter, userId],
+    queryFn: ({ pageParam = 0 }) =>
+      fetchTranscriptions({ pageParam, filter, userId }),
     getNextPageParam: (lastPage) => lastPage.nextCursor,
     initialPageParam: 0,
     initialData: {
       pageParams: [0],
-      pages: [initialData]
-    }
+      pages: [initialData],
+    },
   });
 };
