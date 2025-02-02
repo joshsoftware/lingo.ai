c3a75c0fb52c0d5652e1680bf55022d6b202eb61
Sourabh Bharale <sourabh@Sourabhs-MacBook-Pro.local>
2025-02-02 11:57:07 +0530

    chore: runtime node for ut

--- app/src/app/api/uploadthing/route.ts
+++ app/src/app/api/uploadthing/route.ts
@@ -2,6 +2,7 @@ import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
+export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 export const revalidate = 0;
