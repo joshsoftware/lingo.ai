e38686c31fcb546fc8a64e65aa8c58c8d8394ead
Sourabh Bharale <sourabh@Sourabhs-MacBook-Pro.local>
2025-02-02 12:22:06 +0530

    chore: callback url test

---
+++ app/src/app/api/test_upload/route.ts
@@ -0,0 +1,5 @@
+
+export function GET(req: Request, ) {
+    console.log("GET /api/uploadthing/test_upload", {req});
+    return new Response(JSON.stringify({message: "Hello World"}), {status: 200});
+}
