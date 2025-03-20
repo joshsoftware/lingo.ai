f5dd8e42d6d123fc5d782aa56d3bc2c8b162632c
ShubhamPradhan17 <147041307+ShubhamPradhan17@users.noreply.github.com>
2025-03-20 13:23:33 +0530

    Added Bot and attendee directories in lingo repo.

---
+++ Bot/app/core/config.py
@@ -0,0 +1,10 @@
+from fastapi.security import OAuth2AuthorizationCodeBearer
+
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+CLIENT_SECRETS_FILE = 'credentials.json'
+REDIRECT_URI = "http://localhost:8001/auth/google/callback"
+OAUTH2_SCHEME = OAuth2AuthorizationCodeBearer(
+    tokenUrl="",
+    authorizationUrl="https://accounts.google.com/o/oauth2/auth"
+)
