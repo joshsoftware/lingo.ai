c684cf619f1059bcd988afbda26857c0953d9017
malharjadhav8999 <malharjadhav8999@gmail.com>
2025-04-28 18:24:15 +0530

    sign-out functionality
    py cors

--- Bot/app/main.py
+++ Bot/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from app.api import auth, meetings, scheduler
 from app.core.scheduler import scheduler as apscheduler
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.on_event("startup")
 def startup_event():
--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,28 +3,36 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import TanstackQueryProvider from "@/providers/TanstackQueryProvider";
 import { secondaryFont } from "@/fonts";
+import { CookiesProvider } from "next-client-cookies/server";
 import Header from "@/components/Header";
+import { isSignedIn } from "@/actions/auth";
 
 export const metadata: Metadata = {
   title: "Lingo.ai",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isUserSignedIn = await isSignedIn();
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={`h-screen flex flex-col overflow-y-auto ${secondaryFont.className}`}>
-        <TanstackQueryProvider>
-          <Header />
-          <section className="flex-1 overflow-y-auto">
-            <div className="container h-full">{children}</div>
-          </section>
-          <Toaster richColors closeButton />
-        </TanstackQueryProvider>
+      <body
+        className={`h-screen flex flex-col overflow-y-auto ${secondaryFont.className}`}
+      >
+        <CookiesProvider>
+          <TanstackQueryProvider>
+            <Header isSignedIn={isUserSignedIn} />
+            <section className="flex-1 overflow-y-auto">
+              <div className="container h-full">{children}</div>
+            </section>
+            <Toaster richColors closeButton />
+          </TanstackQueryProvider>
+        </CookiesProvider>
       </body>
     </html>
   );
