cce2d6b206d22a31f00894493ea64509cf8a132a
malharjadhav8999 <malharjadhav8999@gmail.com>
2025-05-05 17:32:40 +0530

    google authentication and calendar acccess bot added logout functionality added bot table added and tokens stored in it isBotAdded flag based code is completed

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import { BotIcon } from "lucide-react";
+import { BotIcon, User, LogOut } from "lucide-react";
 
 import { primaryFont, tertiaryFont } from "@/fonts";
 import { cn } from "@/lib/utils";
@@ -12,19 +12,27 @@ import { usePathname, useRouter } from "next/navigation";
 import { Button, buttonVariants } from "./ui/button";
 import { Modal } from "./ui/modal";
 import { handleSignOut, isSignedIn } from "@/actions/auth";
+import Cookies from "js-cookie";
+import { Popover } from "./ui/popover";
 
 type HeaderProps = {
   isSignedIn: boolean;
 };
 
 const Header = ({ isSignedIn }: HeaderProps) => {
-  // TODO: this flag will come from backend
-  const isBotAdded = false;
+  const router = useRouter();
+
+  const [isBotAdded, setIsBotAdded] = useState(false);
+
+  useEffect(() => {
+    // Read the isBotAdded cookie when the component mounts
+    const botAdded = Cookies.get("isBotAdded") === "true"; // Cookies store the flag as a string
+    setIsBotAdded(botAdded);
+  }, []);
 
   const pathName = usePathname();
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const router = useRouter();
+  const [popoverOpen, setPopoverOpen] = useState(false);
 
   const {
     refetch: fetchAuthLink,
@@ -34,7 +42,7 @@ const Header = ({ isSignedIn }: HeaderProps) => {
     queryKey: ["google-auth"],
     queryFn: async () => {
       const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_BOT_URL}/auth/google`
+        `${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_URL}/auth/google`
       );
       return response.data;
     },
@@ -49,8 +57,8 @@ const Header = ({ isSignedIn }: HeaderProps) => {
     try {
       const { data } = await fetchAuthLink();
 
-      if (data?.authUrl) {
-        window.location.href = data.authUrl;
+      if (data?.auth_url) {
+        window.location.href = data.auth_url;
       } else {
         console.log("Authorization URL not found");
       }
@@ -60,8 +68,14 @@ const Header = ({ isSignedIn }: HeaderProps) => {
   };
 
   const handleLogout = async () => {
-    await handleSignOut();
+    try {
+      await handleSignOut();
+      router.push("/signin");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
   };
+
 
   return (
     <>
@@ -131,19 +145,33 @@ const Header = ({ isSignedIn }: HeaderProps) => {
               </Button>
             )}
             {isSignedIn && (
-              <Button
-                className={cn(
-                  buttonVariants({
-                    className:
-                      "!bg-[#ff0000] !hover:bg-[#668D7E] text-white text-xs px-3",
-                    size: "xs",
-                  }),
-                  tertiaryFont.className
-                )}
-                onClick={handleLogout}
+              <Popover
+                isOpen={popoverOpen}
+                onClose={() => setPopoverOpen(false)}
+                trigger={
+                  <div
+                    onClick={() => setPopoverOpen(!popoverOpen)}
+                    className={cn(
+                      "w-8 h-8 flex items-center justify-center rounded-full bg-white  cursor-pointer transition-all duration-200 hover:shadow-md border",
+                      popoverOpen ? "shadow-lg" : ""
+                    )}
+                  >
+                    <User className="text-black" size={16} />
+                  </div>
+                }
               >
-                Logout
-              </Button>
+                <Button
+                  onClick={handleLogout}
+                  variant="ghost"
+                  className={cn(
+                    "text-xs flex w-full items-center",
+                    tertiaryFont.className
+                  )}
+                >
+                  <LogOut className="mr-1" size={16} />
+                  Logout
+                </Button>
+              </Popover>
             )}
           </div>
         </div>
