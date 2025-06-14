636c4577e5f70fa5b38685e9749f985d3fa89127
sourabh-josh <sourabh.bharale@joshsoftware.com>
2025-06-15 00:13:04 +0530

    reafact(drizzle-connector): pg pooled connection (#90)
    * reafact(drizzle-connector): pg pooled connection
    
    * refact(drizzle-config): override default timeout params
    
    ---------
    
    Co-authored-by: Sourabh Bharale <sourabh@Sourabhs-MacBook-Pro.local>

--- app/src/db/index.ts
+++ app/src/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
-import { Client } from 'pg'
+import { Pool } from 'pg'
 import * as schema from './schema'
 
 
@@ -7,10 +7,11 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: { rejectUnauthorized: false }
+  max: 10, // max 10 connections
+  idleTimeoutMillis: 30000, // idle connections are closed after 30s
+  connectionTimeoutMillis: 2000, // wait 2s for a connection before failing
 })
-await client.connect()
 
-export const db = drizzle(client, { schema })
+export const db = drizzle(pool, { schema })
