a575faf7786c48fc307af09ebf471d9808e5487b
ingale12345 <ingaleprajawal@.com>
2025-06-18 18:16:47 +0530

    removed admin route

--- app/src/app/admin/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-function page() {
-  return <div>Dashboard</div>;
-}
-
-export default page;
--- app/src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Page from "../signin/page";
-
-function page() {
-  return <Page />;
-}
-
-export default page;
