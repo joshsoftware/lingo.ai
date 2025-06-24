efcaa66861c4d1d14268efdbbeef54c7ced76b52
ingale12345 <ingaleprajawal@.com>
2025-06-24 12:18:26 +0530

    changes for..  when user is login then first screen will be transcribe is should not go landing page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { isSignedIn } from "@/actions/auth";
 import Landing from "@/components/Landing";
-export default async function Home() {
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const isUserSignedIn = await isSignedIn();
+      if (isUserSignedIn) {
+        router.push("/new");
+      }
+    };
+    checkAuth();
+  }, [router]);
+
   return (
-    <div className="flex w-full  flex-col ">
+    <div className="flex w-full flex-col">
       <Landing />
     </div>
   );
