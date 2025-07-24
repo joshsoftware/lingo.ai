46a5a3fc82f5246a6fbe0f7a65e6008f78a2f97c
Ravidas Gaikwad <ravidas.gaikwad@joshsoftware.com>
2025-07-24 12:09:05 +0530

    delete the migrations

--- app/migrations/0011_secret_arachne.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "password_reset_tokens" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"user_email" text NOT NULL,
-	"token" text NOT NULL,
-	"expires_at" timestamp NOT NULL,
-	"createdAt" timestamp DEFAULT now()
-);
