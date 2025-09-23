5756af462f35476c6882c3f485a03365b4de20c6
arpita-josh02 <arpita.raut@joshsoftware.com>
2025-09-23 17:23:41 +0530

    remove default cred from file take from env

--- service/banking/database.py
+++ service/banking/database.py
@@ -5,11 +5,11 @@ import datetime
 import os
 
 # PostgreSQL connection string
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "voice_banking")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
