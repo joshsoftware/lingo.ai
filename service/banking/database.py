b98a8001f236298958d708a41490c6d262be47a0
arpita-josh02 <arpita.raut@joshsoftware.com>
2025-09-23 17:38:20 +0530

    move database cred to config file

--- service/banking/database.py
+++ service/banking/database.py
@@ -3,13 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import datetime
 import os
+import config
 
 # PostgreSQL connection string
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = config.db_user
+DB_PASSWORD = config.db_password
+DB_HOST = config.db_host
+DB_PORT = config.db_port
+DB_NAME = config.db_name
+
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
