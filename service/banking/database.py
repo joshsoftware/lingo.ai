16c75af54f67ddb532b6707bae1285a1f8d178d7
arpita-josh02 <arpita.raut@joshsoftware.com>
2025-09-25 12:43:36 +0530

    Feature/banking api's (#149)
    * banking api's
    
    * changes request parm as per frontend
    
    * new changes
    
    * remove default cred from file take from env
    
    * move database cred to config file
    
    * upadted chnages
    
    * added details message in response
    
    * added customer id in api response

---
+++ service/banking/database.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, DateTime, Text, Boolean, Index
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+import datetime
+import os
+import sys
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config  # Direct import from parent directory
+
+# PostgreSQL connection string
+DB_USER = config.db_user
+DB_PASSWORD = config.db_password
+DB_HOST = config.db_host
+DB_PORT = config.db_port
+DB_NAME = config.db_name
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Create PostgreSQL engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a SessionLocal class for database session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a Base class for declarative models
+Base = declarative_base()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
