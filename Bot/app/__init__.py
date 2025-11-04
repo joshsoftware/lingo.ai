3bede0a6ff9af4d554c6f2a8edb94b43860c5648
Yash Maheshwari <yash.maheshwari@joshsoftware.com>
2025-11-04 16:39:53 +0530

    removed attendee, lingo bot and lingo-ui code as it is moved to separate repository

--- Bot/app/__init__.py
+++
@@ -1,3 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv()  # loads from .env
--- cli/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-openai_api_key = os.getenv("OPENAI_KEY")
-#model_id = os.getenv('MODEL_ID', 'large-v3')
-model_id = os.getenv('MODEL_ID')
-model_path = os.getenv('MODEL_PATH')
