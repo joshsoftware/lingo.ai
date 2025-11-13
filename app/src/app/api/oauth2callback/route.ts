00237fd5bc6a9bd73b60f50e6c834e75b15dea57
Josh <omkesh sajjanwar>
2025-11-13 14:22:30 +0530

    URL fixes

--- app/src/app/api/oauth2callback/route.ts
+++ app/src/app/api/oauth2callback/route.ts
@@ -18,7 +18,7 @@ export async function GET(request: Request) {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    redirectUri
+    process.env.GOOGLE_REDIRECT_URI
   );
 
   // Continue handling the request
