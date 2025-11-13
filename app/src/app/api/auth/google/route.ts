2ce990019ec8a71781cc8c91ca20f15efe3be4ce
Josh <omkesh sajjanwar>
2025-11-13 14:14:49 +0530

    Redirect URL added

--- app/src/app/api/auth/google/route.ts
+++ app/src/app/api/auth/google/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: NextRequest) {
     const oauth2Client = new google.auth.OAuth2(
         process.env.GOOGLE_CLIENT_ID,
         process.env.GOOGLE_CLIENT_SECRET,
-        redirectUri
+        process.env.GOOGLE_REDIRECT_URI
     );
 
     const authUrl = oauth2Client.generateAuthUrl({
